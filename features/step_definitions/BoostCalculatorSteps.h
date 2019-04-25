88a36d557a24aa068e617d3369e337f59c7e3b17
Lukas Woodtli <woodtli.lukas@gmail.com>
2019-04-25 16:28:25 +0200

    Add example code

---
+++ features/step_definitions/BoostCalculatorSteps.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <boost/test/unit_test.hpp>
+// Pretend to be GTest
+#define EXPECT_EQ BOOST_CHECK_EQUAL
---
+++ scripts/build_and_run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -u
+set -e
+
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$THIS_SCRIPT_DIR/.."
+
+rm -rf build
+mkdir build
+cd build
+cmake ..
+make -j4
+
